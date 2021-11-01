ae2e62f39f21ebe9aabbb3e7278d786f938e6611
Milagro Scardigno <mdscardigno@gmail.com>
2021-11-01 11:11:21 -0400

    break and continue

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace MultiplicationTable
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table");
+            Console.WriteLine("Please enter the number to use to create the table: ");
+            int table = int.Parse(Console.ReadLine());
+
+            for (int multiplicador = 0; multiplicador < 10; multiplicador++)
+            {
+                if (multiplicador == 5)
+                {
+                    // break; //it exists the loop when equal to 5
+                    continue; // when multiplicador is equal to 5, the line will not be executed
+                }
+                Console.WriteLine(table + " X " + multiplicador + " = " + table * multiplicador);
+            }
+            Console.ReadKey();
+        }
     }
-  }
 }
